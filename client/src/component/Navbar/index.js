bc148f361422881972f57f6faf20cd176abdb7f4
Fourth1755 <fourthe.hero@gmail.com>
2022-08-26 17:06:31 +0700

    update-navbar

--- client/src/component/Navbar/index.js
+++ client/src/component/Navbar/index.js
@@ -2,11 +2,21 @@ import './index.scss'
 const Navbar=()=>{
     return(
         <div className='navbar'>
-            <div className='nav-logo'>
-                <h1 className='nav-logo-text'>AniMap</h1>
+            <div className='navbar-left'>
+                <div className='navbar-logo'>
+                    <h1 className='navbar-logo-text'>AniMap</h1>
+                </div>
+                <div>
+                    <ul className='navbar-item'>
+                        <li className="navbar-item-li"><a href="/" className="navbar-item-link">Home</a></li>
+                        <li className="navbar-item-li"><a href="/" className="navbar-item-link">Anime</a></li>
+                        <li className="navbar-item-li"><a href="/" className="navbar-item-link">Top Anime</a></li>
+                        <li className="navbar-item-li"><a href="/" className="navbar-item-link">My Anime</a></li>
+                    </ul>
+                </div>
             </div>
             <div>
-
+                <h1>Login</h1>
             </div>
         </div>
     )
