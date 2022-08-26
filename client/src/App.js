38ec14f58adc4373f52b7a5089ad093c2e632db0
Fourth1755 <fourthe.hero@gmail.com>
2022-08-26 16:39:39 +0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Redirect, Route ,Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import Navbar from './component/Navbar';
+import React, { useState, useEffect } from "react";
 function App() {
+  const AuthApp=()=>{
+    return (
+    <Routes>
+        <Route path="/" element={<HomePage/>}/>
+    </Routes>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <AuthApp/>
     </div>
   );
 }
---
+++ client/src/component/Navbar/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+const Navbar=()=>{
+    return(
+        <div className='navbar'>
+            <div className='nav-logo'>
+                <h1 className='nav-logo-text'>AniMap</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+export default Navbar;
