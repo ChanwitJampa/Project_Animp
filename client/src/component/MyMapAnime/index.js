8c0b2d656b3602e9228a43407668dc0cd9b05f7d
Fourth1755 <fourthe.hero@gmail.com>
2022-09-03 00:21:08 +0700

    update-mymap-map

--- client/src/component/MyMapAnime/index.js
+++ client/src/component/MyMapAnime/index.js
@@ -1,7 +1,8 @@
 import './index.scss'
 const MyMapAnime=()=>{
     return(<div className="season-year-list">
-        <h2></h2>
+        <h2>My Anime</h2>
+        <h4>Watched in (item)</h4>
     </div>);
 }
 export default MyMapAnime
--- client/src/pages/AnimeMapPage/index.js
+++ client/src/pages/AnimeMapPage/index.js
@@ -1,19 +1,37 @@
 import './index.scss'
 import React, { useRef, useState,useEffect } from "react";
 const AnimeMapPage=()=>{
-    const mainWallpaper= "https://wallpaperaccess.com/full/3097725.jpg"
+    const mainWallpaper= "./image/New_map.png"
     let mapStyle = {
         width: `100%`,
         height: `660px`,
         backgroundImage: `url(${mainWallpaper})`,
         backgroundRepeat: `no-repeat`,
-        backgroundPosition: `center center`,
+        backgroundPosition: `center -300px`,
         backgroundSize: `cover`
       };
     return(
         <div style={mapStyle}>
             <div className='animeMap-text-header'>
                 <h1>My Map</h1> 
+            </div>
+            <div className='linenumber1'>
+                <span class="dot" id="dotnumber1"></span>
+            </div>
+            <div className='linenumber2'>
+                <span class="dot" id="dotnumber2"></span>
+            </div>
+            <div className='linenumber3'>
+                <span class="dot" id="dotnumber3"></span>
+            </div>
+            <div className='linenumber4'>
+                <span class="dot" id="dotnumber4"></span>
+            </div>
+            <div className='linenumber5'>
+                <span class="dot" id="dotnumber5"></span>
+            </div>
+            <div className='progress-box'>
+                <h2>Beginer</h2>
             </div>
         </div>
     );
