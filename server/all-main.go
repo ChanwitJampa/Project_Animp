36d16d61bf8dab4be1c144f09f562965a37dd14c
Artid Yenpram <98255814+justPlai@users.noreply.github.com>
2022-10-03 18:52:46 +0700

    accounts_API
    update

--- server/all-main.go
+++ server/all-main.go
@@ -11,10 +11,10 @@ import (
 )
 
 type Account struct {
-	Accounts_id   int    `db:"accounts_id" json:"accounts_id"`
-	Accounts_name string `db:"accounts_name" json:"accounts_name"`
-	Accounts_user string `db:"accounts_user" json:"accounts_user"`
-	Accounts_pwd  string `db:"accounts_pwd" json:"accounts_pwd"`
+	Id   int    `db:"accounts_id" json:"accounts_id"`
+	Name string `db:"accounts_name" json:"accounts_name"`
+	User string `db:"accounts_user" json:"accounts_user"`
+	Pwd  string `db:"accounts_pwd" json:"accounts_pwd"`
 }
 
 type Anime struct {
@@ -81,7 +81,7 @@ func setupRouter() *gin.Engine {
 	r.GET("/accounts", h.GetAllAccounts)
 	r.GET("/accounts/:id", h.GetAccount)
 	r.POST("/accounts", h.SaveAccount)
-	// r.PUT("/accounts/:id", h.UpdateAccount)
+	r.PUT("/accounts/:id", h.UpdateAccount)
 	r.DELETE("/accounts/:id", h.DeleteAccount)
 
 	return r
@@ -109,7 +109,7 @@ func (h *AnimapHandler) GetAllAccounts(c *gin.Context) {
 
 	for rows.Next() {
 		var a Account
-		err = rows.Scan(&a.Accounts_id, &a.Accounts_name, &a.Accounts_user, &a.Accounts_pwd)
+		err = rows.Scan(&a.Id, &a.Name, &a.User, &a.Pwd)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -123,7 +123,11 @@ func (h *AnimapHandler) GetAccount(c *gin.Context) {
 	id := c.Param("id")
 	account := Account{}
 	row := h.DB.Table("accounts").Where("accounts_id = ?", &id).Select("accounts_id", "accounts_name", "accounts_user", "accounts_pwd").Row()
-	row.Scan(&account.Accounts_id, &account.Accounts_name, &account.Accounts_user, &account.Accounts_pwd)
+	if err := row.Err(); err != nil {
+		log.Fatal(err)
+	}
+	row.Scan(&account.Id, &account.Name, &account.User, &account.Pwd)
+
 	c.JSON(http.StatusOK, account)
 
 }
@@ -143,6 +147,40 @@ func (h *AnimapHandler) SaveAccount(c *gin.Context) {
 
 }
 
+func (h *AnimapHandler) UpdateAccount(c *gin.Context) {
+	id := c.Param("id")
+	account := Account{}
+	row := h.DB.Table("accounts").Where("accounts_id = ?", &id).Select("accounts_id", "accounts_name", "accounts_user", "accounts_pwd").Row()
+	if err := row.Err(); err != nil {
+		c.Status(http.StatusNotFound)
+		return
+	}
+	row.Scan(&account.Id, &account.Name, &account.User, &account.Pwd)
+
+	var newAccount = Account{}
+	if err := c.BindJSON(&newAccount); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	if newAccount.Name != "" {
+		account.Name = newAccount.Name
+	}
+	if newAccount.User != "" {
+		account.User = newAccount.User
+	}
+	if newAccount.Pwd != "" {
+		account.Pwd = newAccount.Pwd
+	}
+	if err := h.DB.Exec("UPDATE `accounts` SET `accounts_name` = ? ,`accounts_user` = ? ,`accounts_pwd` = ? WHERE `accounts_id` = ?;", account.Name, account.User, account.Pwd, id).Error; err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, "update success")
+
+}
+
 func (h *AnimapHandler) DeleteAccount(c *gin.Context) {
 	id := c.Param("id")
 	account := Account{}
@@ -159,49 +197,3 @@ func (h *AnimapHandler) DeleteAccount(c *gin.Context) {
 
 	c.JSON(http.StatusOK, "delete success")
 }
-
-// // getAccounts responds with the list of all Accounts as JSON.
-// func getAccounts(c *gin.Context) {
-// 	var accounts []Account
-// 	rows, err := db.Query("select accounts_id, accounts_name, accounts_user, accounts_pwd from accounts")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer rows.Close()
-// 	for rows.Next() {
-// 		var a Account
-// 		err := rows.Scan(&a.Id, &a.Name, &a.User, &a.Pwd)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		// log.Println(id, name, user, pwd)
-// 		accounts = append(accounts, a)
-// 	}
-// 	err = rows.Err()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	c.IndentedJSON(http.StatusOK, accounts)
-// }
-
-// // create
-// func createAccount(c *gin.Context) {
-// 	Name := "plaitest"
-// 	User := Name
-// 	Pwd := Name
-// 	rows, err := db.Query("INSERT INTO animemapdb.accounts (`accounts_name` ,`accounts_user` ,`accounts_pwd`) VALUES (? ,? ,?);", Name, User, Pwd)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer rows.Close()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	err = rows.Err()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	c.IndentedJSON(http.StatusOK, "insert success")
-// }
