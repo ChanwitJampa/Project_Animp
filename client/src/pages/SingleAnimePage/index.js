224de0278bfe0430ded452170110c89aecb27a14
Fourth1755 <fourthe.hero@gmail.com>
2022-10-04 20:10:01 +0700

    update-SingleAnimePage

--- client/src/pages/SingleAnimePage/index.js
+++ client/src/pages/SingleAnimePage/index.js
@@ -1,22 +1,37 @@
 import './index.scss'
 import { useEffect, useState } from "react";
 import { Routes, Route, useParams } from "react-router-dom";
-import Dataanime from "../../assets/anime.json"
 import SliderAnime from '../../component/SliderAnime';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import {useDispatch,useSelector} from 'react-redux'
+import {fetchAnimeAsync} from '../../actions/animeListAction'
+
 const SingleAnimePage=(props)=>{
     let params = useParams();
     const navigate =useNavigate()
-    const singleAnime=Dataanime.filter((item)=>{if(item.id==params.id) return item})
+    const dispatch=useDispatch()
+    const Dataanime =useSelector(state=>state.animeList)
+    const fetchAnime = async () => {
+        await axios.get(`http://192.168.88.237:5000/animes/${params.id}`).
+        then((response) => response.data)
+        .then((anime) => {
+            console.log(anime);     
+            setSingleAnime(anime)
+        }).catch(error=>{
+            console.log(error); 
+        })
+    }
+    const [singleAnime,setSingleAnime] =useState("")
+    //console.log(singleAnime)
     let dropzoneStyle = {
         width: `100%`,
         height: `660px`,
-        backgroundImage: `url(${singleAnime[0].wallpaper})`,
+        backgroundImage: `url(${singleAnime.animes_wallpaper})`,
         backgroundRepeat: `no-repeat`,
         backgroundPosition: `center center`,
         backgroundSize: `cover`
       };
-    const [isAutoPlay, setIsAutoPlay]=useState(false)
     // const handleScroll = () => {
     //     const position = window.pageYOffset;
     //     setScrollPosition(position);
@@ -30,28 +45,26 @@ const SingleAnimePage=(props)=>{
     
     useEffect(()=>{
         window.scrollTo(0, 0);
-        const myTimeout = setTimeout(setAutoPlay, 3000);
+        fetchAnime()
+        dispatch(fetchAnimeAsync())
     },[])
-    const setAutoPlay=()=>{
-        setIsAutoPlay(true)
-    }
-    const animeList = Dataanime.filter((item)=>{if(item.studios==singleAnime[0].studios) return item})
+    const animeList = Dataanime.filter((item)=>{if(item.Studio==singleAnime.Studio) return item})
 
     return(
         <div>
             <div style={dropzoneStyle}>
                 <div className='singleAnime-wallpaper'>
                     <div className='singleAnime-wallpaper-text'>
-                        <h1>{singleAnime[0].name}</h1>
+                        <h1>{singleAnime.animes_name}</h1>
                     </div>
                     <div className='singleAnime-wallpaper-detailbox'>
                         <div className='singleAnime-wallpaper-detailbox-imagebox'>
-                            <img src={singleAnime[0].image}></img>
+                            <img src={singleAnime.animes_image}></img>
                             <button>Add to list</button>
                         </div>
                         <div className='singleAnime-wallpaper-detailbox-tagAnime'>
                             <div className='detailbox-tagAnime-score'>
-                                <h2>Score {singleAnime[0].score}/10</h2>
+                                <h2>Score {singleAnime.animes_score}/10</h2>
                                 <h2>My score /10</h2><br></br>
                             </div>
                             <div className='detailbox-tagAnime-score'>
@@ -62,7 +75,7 @@ const SingleAnimePage=(props)=>{
                         </div>
                         <div className='videoWrapper-container'>
                             <div className='videoWrapper'>
-                                <iframe width="560" height="315" id="player" src={`${singleAnime[0].trailer}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <iframe width="560" height="315" id="player" src={`${singleAnime.animes_trailer}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                         </div>
                     </div>
@@ -71,14 +84,14 @@ const SingleAnimePage=(props)=>{
             <div className='anime-detail-container'>
                 <div className='anime-detail'>
                     <h2>Detail</h2><br/>
-                    <p>Anime: {singleAnime[0].name}</p><br/>
-                    {singleAnime[0].seasonal?<>
-                        <p>Episodes: {singleAnime[0].episodes}</p><br/>
-                        <p>Seasonal: {singleAnime[0].seasonal}</p></>:
+                    <p>Anime: {singleAnime.animes_name}</p><br/>
+                    {singleAnime.animes_episodes==1?<>
+                        <p>Episodes: {singleAnime.animes_episodes}</p><br/>
+                        <p>Seasonal: {singleAnime.animes_seasonal}</p></>:
                     <p>Type: Movie</p>}<br/>
-                    <p>Year: {singleAnime[0].year}</p><br/>
-                    <p onClick={()=>navigate(`/studio/${singleAnime[0].id}`)}>Studio: {singleAnime[0].studios}</p><br/>
-                    <p>Duration: {singleAnime[0].duration}</p>
+                    <p>Year: {singleAnime.animes_year}</p><br/>
+                    <p onClick={()=>navigate(`/studio/${singleAnime.animes_id}`)}>Studio: {singleAnime.Studio}</p><br/>
+                    <p>Duration: {singleAnime.animes_duration}</p>
                 </div>
             </div>
             <SliderAnime tagAnime="From same studio" animeList={animeList}/>
