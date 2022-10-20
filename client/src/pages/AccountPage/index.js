c9755e17caab60d520ac8c7eb8fe1aff9afee14e
Fourth1755 <fourthe.hero@gmail.com>
2022-10-20 09:50:27 +0700

    update-account

--- client/src/pages/AccountPage/index.js
+++ client/src/pages/AccountPage/index.js
@@ -10,21 +10,35 @@ import { Elements, } from "@stripe/react-stripe-js";
 import { loadStripe } from '@stripe/stripe-js';
 import { publishableKeyGet } from '../../payment/constants/functions'
 import PaymaentModal from "../../payment/PaymentForm";
+import axios from "axios";
 const AccountPage = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => {
       setOpen(true);
   }
+  const [mycoin,setMycoin]=useState('')
   const handleClose = () =>setOpen(false);
   const user=getUser()
+  const fetchMyCoin=()=>{
+     axios.get(`http://localhost:5000/api/myCoin/${user.ID}`).
+        then((response) => response.data)
+        .then((coin) => {
+          setMycoin(coin)
+        }).catch(error=>{
+            console.log(error); 
+        })
+  }
+  useEffect(()=>{
+    fetchMyCoin()
+  },[])
   return (
     <div>
       <div className="accountpage-header">
         <h1>My Account</h1>
       </div>
       <div className="accountpage-container">
-        <div className="accountpage-detail"><h1>{user.Name}</h1><h2>Usename :</h2></div>
-        <div className="accountpage-coin"><h2>My coin: 0</h2><button onClick={handleOpen}>Add coin</button></div>
+        <div className="accountpage-detail"><h1>{user.Name}</h1><h2>Usename :{user.Email}</h2></div>
+        <div className="accountpage-coin"><h2>My coin: {mycoin}</h2><button onClick={handleOpen}>Add coin</button></div>
       </div>
       <PaymaentModal open={open} onClose={handleClose}/>
     </div>
--- client/src/pages/StudioPage/index.js
+++ client/src/pages/StudioPage/index.js
@@ -23,7 +23,6 @@ const StudioPage=()=>{
                         <th className='table-header-20'>Studio</th>
                         <th>Name</th>
                         <th className='table-header-10'>Established</th>
-                        <th className='table-header-10'>Donate</th>
                         <th className='table-header-10'>Detail</th>
                     </tr> 
                 </thead>
@@ -37,7 +36,6 @@ const StudioPage=()=>{
                             onClick={()=>navigate(`/studio/${studio.studioes_name}`)}></div></td>
                             <td><h1>{studio.studioes_name}</h1></td>
                             <td>{studio.studioes_established}</td>
-                            <td><button className='table-content-button'>Donate</button></td>
                             <td><button className='table-content-button' onClick={()=>navigate(`/studio/${studio.studioes_name}`)}>Detail</button></td>
                         </tr>)
                     })}
